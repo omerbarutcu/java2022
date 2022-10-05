afea13f98fa0c3bf201c2b701bebef7d6db3f468
Omer Barutcu <omerbrtc@hotmail.com>
2022-10-06 00:54:06 +0300

    3. ders sonu

---
+++ oopWithNLayeredApp/src/Main.java
@@ -0,0 +1,18 @@
+import business.ProductManager;
+import core.logging.DatabaseLogger;
+import core.logging.FileLogger;
+import core.logging.Logger;
+import core.logging.MailLogger;
+import dataAccess.JdbcProductDao;
+import entities.Product;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Product product1 = new Product(1, "Iphone 7", 15);
+        Logger[] loggers = {new DatabaseLogger(),new FileLogger(),new MailLogger()};
+
+        ProductManager productManager = new ProductManager(new JdbcProductDao(),loggers);
+        productManager.add(product1);
+    }
+}
